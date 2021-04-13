d02e4f6644cc7aac281c4b34f7d42c43f4673d33
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-04-13 13:55:23 +0300

    Basic operations with C#

--- Program.cs
+++ Program.cs
@@ -9,7 +9,19 @@ namespace GradeBook
             double x = 34.1;
             double y = 12.2;
 
+            double[] numbers = new double[3];
+            numbers[0] = 5.25;
+            numbers[1] = 4.55;
+            numbers[2] = 5.85;
+
+            double sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            }
+
             Console.WriteLine(x + y);
+            System.Console.WriteLine(sum);
         }
     }
 }
