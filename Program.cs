b223490b2925ef9a687c14d4bdcd8d749e436cde
TsvetanovM <tsvetanov.martin2@gmail.com>
2021-04-14 15:05:52 +0300

    Creating classes in C#

--- Program.cs
+++ Program.cs
@@ -10,11 +10,20 @@ namespace GradeBook
             List<double> grades = new List<double>() {5.25, 3.4, 4.92, 5.67};
             double totalGrades = 0;
 
+            Book book = new Book();
+
             foreach(double grade in grades) {
                 totalGrades += grade;
             }
 
             System.Console.WriteLine($"The average grade is {totalGrades / grades.Count:N2}");
         }
+    }
+
+    class Book {
+        private string name;
+        private int pages;
+
+        public Book() {}
     }
 }
